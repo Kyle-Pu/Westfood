8bb38aae161ad51458758eb2a5934873dc65931b
Kyle Pu <kylez.pu@gmail.com>
2021-11-08 16:07:46 -0800

    Registration page MVP complete.
    First name, last name, email, and password fields created for the account registration page. Currently does nothing on submit. Data saving (and eventual fetching for the login page) to be handled by Prateek and Preetham.

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,23 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from "../pages/footer"
 import Header from "../pages/header"
 
-const Register = (props) => {
-    return (
-        <div>
-            <Header title="Register"></Header>
+class Register extends React.Component{
 
-            <br />
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        };
 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event){
+        // Backend stuff
+        // Save data and whatnot
+    }
+
+    handleChange(event){
+        const {target} = event;
+        const {value} = target;
+        const {name} = target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render(){
+        return (
             <div>
-                put text here
+                <Header title="Register"></Header>
+
+                <br />
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        First Name:
+                        <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Last Name:
+                        <input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Email: 
+                        <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input name="password" type="text" value={this.state.password} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <br />
+                    <input type="submit" value="Create Account" />
+                </form>
+
+                <br />
+
+                <Footer></Footer>
             </div>
-
-            <br />
-
-            <Footer></Footer>
-        </div>
-    )
+        );
+    }
 }
 
 export default Register
