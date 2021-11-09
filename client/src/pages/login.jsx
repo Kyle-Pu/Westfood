8675f4462298df9518b2b87b5f606d0217fc4bec
Kyle Pu <kylez.pu@gmail.com>
2021-11-08 16:24:21 -0800

    Login page MVP
    Email and password field created for the login page. No data fetching to verify account is done yet.

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,22 +2,68 @@ import React from 'react'
 import Footer from "../pages/footer"
 import Header from "../pages/header"
 
-const Login = (props) => {
-    return (
-        <div>
-            <Header title="Login"></Header>
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            email: "",
+            password: ""
+        }
 
-            <br />
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
+    handleSubmit(event){
+        // Backend stuff
+        // Fetch email and password
+            // Check for a match
+                // If match
+                    // Login
+                // Else
+                    // Display message to user to re-enter information
+    }
+
+    handleChange(event){
+        const {target} = event;
+        const {value} = target;
+        const {name} = target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render(){
+        return (
             <div>
-                put text here
+                <Header title="Login"></Header>
+
+                <br />
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email:
+                        <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <br />
+                    <input type="submit" value="Sign in"/>
+                </form>
+
+                <br />
+
+                <Footer></Footer>
             </div>
+        );
+    };
 
-            <br />
-
-            <Footer></Footer>
-        </div>
-    )
 }
 
 export default Login;
