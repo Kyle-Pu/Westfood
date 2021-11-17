1916f682be8bbcc52af2e90d83ed0acf8922a88f
Preetham Pangaluru <preethamvignesh@Preethams-MacBook-Pro.local>
2021-11-17 13:34:41 -0800

    I added the reviews schema and implemented the reviews part in the routes.js . There is still a dependency between the restaurant/user and their reviews that need to be addressed (coming soon). Otherwise the schema and the part in Review.js appears to be good.

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,34 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const User = require('./schemas/User.js')
 const Restaurant = require('./schemas/Restaurant.js')
+const Review = require('./schemas/Review.js')
+
+//This part is for posting a review
+router.post('/review', (req,res) => {
+    console.log("ping /review review")
+    //check for missing fields
+    if(!req.query.description || !req.query.rating){
+        res.status(400).json({error: 'missing a required field' })
+    }
+    else{
+        Review.create({
+            description: req.query.description,
+            rating: req.query.rating
+        }, (err, response) => {
+            if (err){
+                console.log(err);
+                res.status(400).json({ error: 'error in posting a review'});
+            }
+            if (response){
+                console.log(response);
+                res.status(200).json({ success: 'created a review'});
+            }
+        })
+
+    }
+}
+
+)
 
 router.post('/user', (req, res) => {
     console.log("ping /user post")
@@ -17,7 +45,10 @@ router.post('/user', (req, res) => {
             firstName: req.query.firstName,
             lastName: req.query.lastName
         }, (err, response) => {
-            if (err) console.log(err);
+            if (err){
+                console.log(err);
+                res.status(400).json({ error: 'error in creating an account' });
+            } 
             if (response) {
                 console.log(response);
                 res.status(200).json({ success: 'created user' })
