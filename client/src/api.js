a421517ed25f45d47b1c7dbc35796044c357e354
jakegreenbergbell <32340594+jakegreenbergbell@users.noreply.github.com>
2021-11-17 21:57:00 -0800

    Added api.js file.
    Added the file to the repository which contains the frontend api calls.

---
+++ client/src/api.js
@@ -0,0 +1,52 @@
+import axios from "axios"
+
+// create user object, accepts JSON with all neede parameters
+const addUser = async (body) => {
+    return await axios.post('http://localhost:8000/user', body)
+    .then(res => {
+        console.log(res);
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
+        return err;
+    });
+}
+
+// get user object
+const getUsers = async () => {
+    return await axios('http://localhost:8000/users')
+    .then(res => {
+        console.log(res);
+        return res;
+    })
+    .catch(err => console.log(err));
+}
+
+// create restuarant object
+const addRestaurant = async (body) => {
+    return await axios.post('http://localhost:8000/restaurant', body)
+    .then( res => {
+        console.log(res);
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
+        return err;
+    })
+}
+
+// get restaurant object
+const getRestaurants = async () => {
+    return await axios('http://localhost:8000/restaurants')
+    .then( res => {
+        console.log(res);
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
+        return err;
+    })
+}
+
+export {addUser, getUsers, addRestaurant, getRestaurants};
