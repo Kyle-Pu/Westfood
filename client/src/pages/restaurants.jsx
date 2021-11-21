4b78573f68efe838f408a6cea57f7c36b4e6c149
Kyle Pu <kylez.pu@gmail.com>
2021-11-21 15:26:36 -0800

    Restaurant names are all taken from backend/server/database now.
    Code refactored to properly display restaurant info boxes when its corresponding button is clicked. Uses effect hook (triggered only on component mount) to fetch data from backend.
    
    Many console.log() statements still in code for debugging/verification purposes.

--- client/src/pages/restaurants.jsx
+++ client/src/pages/restaurants.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import * as api from "../api.js"
 
 const RestaurantInfoBox = (props) => {    
+
+    const submitReview = () => {
+        // Backend stuff
+    }
+
     return (
         <div>
             <h1>{props.name}</h1>
@@ -15,31 +21,45 @@ const RestaurantInfoBox = (props) => {
                 <li>bussin<p>11/15/2021</p></li>
                 <li>shit<p>11/16/2021</p></li>
             </ul>
+
+            <textarea>Write review here</textarea>
+            <br />
+            <button onClick={submitReview}>Submit Review</button>
         </div>
     );
 }
 
-const RestaurantsPage = () =>{
+const RestaurantsPage = () => {
 
-    let restaurants = 
-        ["Tocaya Modern Mexican",
-        "La Table de Sophie",
-        "Mr. Noodle",
-        "Skylight Gardens",
-        "Blue Bird Burgers",
-        "Pomodoro Trattoria",
-        "Hibachi Papi",
-        "Gogobop"];
-    let [restaurantClicked, setRestaurantClicked] = useState(Array(restaurants.length).fill(false)); // Array to keep track of who's button has been pressed
+    let [restaurants, setRestaurants] = useState([]);
+    let [restaurantClicked, setRestaurantClicked] = useState([]); // Array to keep track of who's button has been pressed
+
+    useEffect(() => {
+        api.getRestaurants().then(data => {
+            let rests = data['data'].map(element => element.name)
+            setRestaurants(rests)
+            setRestaurantClicked(Array(rests.length).fill(false)) // https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
+        });
+    }, []);
+        
+    // Demonstrates how to see the actual data in our console
+    // (async () => {
+    //    restaurantsData = await api.getRestaurants()
+    //    let restaurants = restaurantsData['data']
+    //    console.log(restaurants)
+    //})()
 
     const handleClick = (event) => {
         // Toggle bool value of clicked restaurant
+        console.log(restaurants)
         setRestaurantClicked(restaurantClicked.map((x, ind) => {
             if(ind == event.target.name){
+                console.log("Here")
                 return !x;
             }
             return x;
         }));
+        console.log(restaurantClicked)
     }
 
     let restaurantButtons = restaurants.map((user, idx) => {
@@ -56,10 +76,11 @@ const RestaurantsPage = () =>{
             {restaurantButtons}
 
             {restaurants.map((nm, idx) => {
-                return restaurantClicked[idx] ? <RestaurantInfoBox name={nm} /> : <br/>
+                return restaurantClicked[idx] && <RestaurantInfoBox name={nm} />
             })}
         </div>
     );
+    
 };
 
 export default RestaurantsPage;
